00cd969f2ce3dbf9b5c68dc6fac2032f9bd0fe25
M Nadeem Asghar <m.nadeem.asghar@hotmail.com>
2024-01-25 03:39:13 +0500

    table added in donations page

--- resources/views/donations.blade.php
+++ resources/views/donations.blade.php
@@ -5,16 +5,26 @@
         </h2>
     </x-slot>
 
-    @foreach ($data as $donation)
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <p>
-                        {{ $donation->blood_type }}
-                        {{ $donation->name }}
-                    </p>
-                </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <table class="min-w-full">
+                    <thead>
+                        <tr>
+                            <td class="py-2 px-4 border-b">Blood Type</td>
+                            <td class="py-2 px-4 border-b">Name</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data as $donation)
+                            <tr>
+                                <td class="py-2 px-4 border-b">{{ $donation->blood_type }}</td>
+                                <td class="py-2 px-4 border-b">{{ $donation->name }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
-    @endforeach
+    </div>
 </x-app-layout>
