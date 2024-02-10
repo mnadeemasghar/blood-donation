967586ce4ba3b0e5f72fa7e71be84fd63be00c82
M Nadeem Asghar <m.nadeem.asghar@hotmail.com>
2024-02-11 03:33:30 +0500

    pak cities helper

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Helpers' => App\Helpers\Helpers::class,
     ])->toArray(),
 
 ];
