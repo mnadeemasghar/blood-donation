4dc6dff2390e507b1502cf203adebe405c2547f5
M Nadeem Asghar <m.nadeem.asghar@hotmail.com>
2024-02-11 00:19:56 +0500

    dashboard changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+                <!-- <x-welcome /> -->
+                <div class="m-4">
+                    {{__("Looking for:")}}
+                    <x-link :href="route('blood_donations.blood_type',['blood_type'=>'A+'])">A+</x-link>
+                    <x-link :href="route('blood_donations.blood_type',['blood_type'=>'A-'])">A-</x-link>
+                    <x-link :href="route('blood_donations.blood_type',['blood_type'=>'B+'])">B+</x-link>
+                    <x-link :href="route('blood_donations.blood_type',['blood_type'=>'B-'])">B-</x-link>
+                    <x-link :href="route('blood_donations.blood_type',['blood_type'=>'AB+'])">AB+</x-link>
+                    <x-link :href="route('blood_donations.blood_type',['blood_type'=>'AB-'])">AB-</x-link>
+                    <x-link :href="route('blood_donations.blood_type',['blood_type'=>'O+'])">O+</x-link>
+                    <x-link :href="route('blood_donations.blood_type',['blood_type'=>'O-'])">O-</x-link>
+                </div>
             </div>
         </div>
     </div>
